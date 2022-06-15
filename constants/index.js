0b07bc00561f478016f47d268496f9235eb5c42d
Romain Noeppel <romain_noeppel@hotmail.com>
2022-06-15 21:13:26 +0200

    update 15/06

---
+++ constants/index.js
@@ -0,0 +1,22 @@
+export const abi =[
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "name": "whitelistedAddresses",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+]
+export const NFT_CONTRACT_ADDRESS = "0xaA0b0515bD7f2fCd8C2a1Ab79e06f2d5790AAE74"
